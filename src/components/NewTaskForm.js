4cb1eefde382b68cf7003ea8a93611fa63143215
Anna Harder <annaqharder@gmail.com>
2022-11-28 15:22:39 -0700

    commit tree a2845e176955cd497d5daf7b77d9af893494edad

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState(""); 
+  const [category, setCategory] = useState("Code"); 
+
+
+  function handleCategory(event) {
+    setCategory(event.target.value)
+  }
+
+  function handleInput(event) {
+    setText(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onTaskFormSubmit({ text, category}); 
+    setText("");
+    setCategory("Code");
+  }
+
+  const optionsElement = categories.map((cat) => (
+    <option key={cat}>{cat}</option>
+  ))
+
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input 
+          type="text" 
+          name="text"
+          value={text}
+          onChange={handleInput}/>
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select 
+          value={category} 
+          name="category" 
+          onChange={handleCategory}
+        >
+          {optionsElement}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ taskData, onDeleteTask}) {
+  const taskList = taskData.map((task) => (
+    <Task 
+        key={task.text}
+        text={task.text}
+        category={task.category}
+        onDeleteTask={onDeleteTask}
+      />
+  ))
+
+    return <div className="tasks">{taskList}</div>;
 }
 
 export default TaskList;
